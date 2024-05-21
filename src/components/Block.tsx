acb6afdb3c92ccff68130ba20fc5081eef8585e0
niranjan <niranjan@naya.studio>
2024-05-21 18:47:59 +0530

    Vertical stack

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,17 +1,14 @@
-import { Draggable } from "react-beautiful-dnd";
-import { BlockType } from "../App";
+import { Draggable, Droppable } from "react-beautiful-dnd";
+import { BlockOrStackType, ItemTypes } from "../App";
 
 type BlockProps = {
-  data: BlockType;
+  data: BlockOrStackType;
   index: number;
+  isStack?: boolean
+  parentId: string;
 };
 
-export const Block = ({ data, index }: BlockProps) => {
-  const isStack =
-    data.fromIndex &&
-    data.toIndex &&
-    (data.fromIndex >= index || data.toIndex <= index);
-
+export const Block = ({ data, index, isStack = false, parentId }: BlockProps) => {
   return (
     <Draggable
       draggableId={data.id}
@@ -19,21 +16,33 @@ export const Block = ({ data, index }: BlockProps) => {
       key={data.id}
       // isDragDisabled={!!isStack}
     >
-      {(provided, snapshot) => {
-        console.log(snapshot);
+      {(provided) => {
         return (
           <div
+            ref={provided.innerRef}
+            {...provided.dragHandleProps}
+            {...provided.draggableProps}
             className="block-wrapper"
-            style={{ backgroundColor: isStack ? "GrayText" : "transparent" }}
           >
-            <div
-              ref={provided.innerRef}
-              {...provided.dragHandleProps}
-              {...provided.draggableProps}
-              className="block"
+            <div className="block">{data.name}</div>
+            <Droppable
+              droppableId={`droppable-${parentId}-${index+1}`}
+              direction="horizontal"
+              type={!isStack ? 'STACK' : ItemTypes.BLOCK}
             >
-              {data.name}
-            </div>
+              {(provided, snapshot) => {
+                return (
+                  <div
+                    className={`droppable ${isStack ? "stack-droppable" : 'phase-droppable'}`}
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                    style={{ textAlign: 'center', backgroundColor: snapshot.isDraggingOver ? 'green' : 'transparent' }}
+                  >
+                    droppable
+                  </div>
+                );
+              }}
+            </Droppable>
           </div>
         );
       }}
